aa8ac53198a6e731b245f072af4c76ae7e6766d5
Ben Mangelsen <bmangelsen@gmail.com>
2017-01-19 19:45:56 -0500

    Wrote method to solve first test

--- ping_pong.rb
+++ ping_pong.rb
@@ -1,11 +1,26 @@
 require 'rspec'
+require 'pry'
+
+def anagram?(word_one, word_two)
+  word_one_chars = word_one.split('')
+  word_two_chars = word_two.split('')
+
+  word_one_chars.sort!
+  word_two_chars.sort!
+
+  if word_one_chars == word_two_chars
+    true
+  else
+    false
+  end
+end
 
 describe "PingPong" do
   it "should return false if the two words are not anagrams" do
-    expect(anagram?("iron", "yard")).to be_false
+    expect(anagram?("iron", "yard")).to be false
   end
 
   it "should return true if the two words are anagrams" do
-    expect(anagram?("iceman", "cinema")).to be_true
+    expect(anagram?("iceman", "cinema")).to be true
   end
 end
